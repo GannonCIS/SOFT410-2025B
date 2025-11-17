66a9da5c5bd72d565d86360800b501d71e1e3369
Olive <oliverjkonzel@gmail.com>
2025-11-16 21:13:00 -0500

    Working version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        boolean useProd = Arrays.stream(args)
-                .anyMatch(arg -> "--prod".equalsIgnoreCase(arg) || "prod".equalsIgnoreCase(arg));
+        boolean useProd = true;
 
         ATM atm = useProd ? AppConfig.prodATM() : AppConfig.devATM();
         atm.getLogin();
